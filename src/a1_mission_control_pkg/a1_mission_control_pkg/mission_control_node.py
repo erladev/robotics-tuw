b4c1a0333dbe3e3e10fbe2f1b43c469f4493e59d
laurin <laurin@erlacher.dev>
2024-12-10 00:26:01 +0100

    working gui node

--- src/a1_mission_control_pkg/a1_mission_control_pkg/mission_control_node.py
+++ src/a1_mission_control_pkg/a1_mission_control_pkg/mission_control_node.py
@@ -5,40 +5,24 @@ from threading import Thread
 from rclpy.executors import MultiThreadedExecutor
 from rclpy.node import Node
 from std_msgs.msg import String
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QWidget, QPushButton
+import PyQt5
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QWidget, QPushButton, QMainWindow
 
-class GuiNode(Node):
+class MyGuiNode(Node):
     def __init__(self):
         super().__init__("simple_gui_node")
-        self.counter_    = 0
-        self.button1_publisher_  = self.create_publisher(String, '/app/button_1', 10)
-        self.button2_publisher_  = self.create_publisher(String, '/app/button_2', 10)
 
-    def button_1_publisher_callback_(self):
-        self.counter_ = self.counter_ + 1
-        msg = String()
-        msg.data = "Button 1 pressed"
-        self.button1_publisher_.publish(msg)
-
-    def button_2_publisher_callback_(self):
-        self.counter_ = self.counter_ + 1
-        msg = String()
-        msg.data = "Button 2 pressed"
-        self.button2_publisher_.publish(msg)
-
-    def subscriber_callback_(self, msg):
-        self.get_logger().info("Button clicked, " + msg.data)
 
 
 def main(args=None):
     rclpy.init(args=args)
     
-    app = QtWidgets.QApplication(sys.argv)
-    HMI = QtWidgets.QMainWindow()
-    ui  = Ui_MainWindow()
-    ui.setupUi(HMI)
+    app = QApplication(sys.argv)
+    HMI = QMainWindow()
+    #ui  = Ui_MainWindow()
+    #ui.setupUi(HMI)
 
-    hmi_node = MyGuiNode(ui)
+    hmi_node = MyGuiNode()
     executor = MultiThreadedExecutor()
     executor.add_node(hmi_node)
 
--- src/a1_mission_control_pkg/setup.py
+++ src/a1_mission_control_pkg/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'mission_control_node = a1_mission_control_pkg.mission_control_node:main'
         ],
     },
 )
