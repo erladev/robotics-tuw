e8b45050587fac7c845b026ee6c2de73ea071073
laurin <laurin@erlacher.dev>
2025-02-27 00:51:11 +0100

    some prebuild steps + gui setup automation

--- gui-docker-fix.bash
+++ gui-docker-fix.bash
@@ -1,2 +1,3 @@
 XAUTH_COOKIES=$(xauth list | head -n 1 | sed 's/greater-demon\/unix:/:0/')
-docker compose exec ros-desktop-full xauth add $XAUTH_COOKIES
+
+docker compose exec ros-desktop-full touch /root/.Xauthority && xauth add $XAUTH_COOKIES
