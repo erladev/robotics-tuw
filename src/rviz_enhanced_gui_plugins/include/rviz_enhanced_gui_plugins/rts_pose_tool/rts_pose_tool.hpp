8e80d9a3975ed3af232e332524259fe5eb4664e7
laurin <laurin@erlacher.dev>
2025-03-07 00:23:47 +0100

    nice drone ui, publish tbd!

--- src/rviz_enhanced_gui_plugins/include/rviz_enhanced_gui_plugins/rts_pose_tool/rts_pose_tool.hpp
+++ src/rviz_enhanced_gui_plugins/include/rviz_enhanced_gui_plugins/rts_pose_tool/rts_pose_tool.hpp
@@ -16,6 +16,7 @@
 #include "rviz_common/display_context.hpp"
 #include "rviz_rendering/viewport_projection_finder.hpp"
 #include "rviz_rendering/objects/shape.hpp"
+#include "rviz_rendering/objects/billboard_line.hpp"
 
 #include "tf2_msgs/msg/tf_message.hpp"
 #include <tf2_ros/message_filter.h>
@@ -23,6 +24,32 @@
 
 #include "rviz_enhanced_gui_plugins/visibility_control.hpp"
 
+/*
+>>> a
+(0.5625, 0.23828125, 0.171875)
+>>> b
+(0.109375, 0.34765625, 0.39453125)
+>>> c
+(0.88671875, 0.953125, 0.96484375)
+>>> d
+(0.05859375, 0.19140625, 0.21875)
+*/
+
+#define COLOR1_R 0.88671875
+#define COLOR1_G 0.37109375
+#define COLOR1_B 0.265625
+
+#define COLOR2_R 0.21875
+#define COLOR2_G 0.64453125
+#define COLOR2_B 0.7265625
+
+#define COLOR3_R 0.88671875
+#define COLOR3_G 0.953125
+#define COLOR3_B 0.96484375
+
+#define COLOR4_R 0.05859375
+#define COLOR4_G 0.19140625
+#define COLOR4_B 0.21875
 
 namespace rviz_rendering {
     class BillboardLine;
@@ -45,13 +72,19 @@ namespace rviz_enhanced_gui_plugins {
             std::shared_ptr<rviz_rendering::ViewportProjectionFinder> projection_finder_;
 
             Ogre::SceneNode * scene_node_;
-            // std::shared_ptr<rviz::BillboardLine> height_indicator_line_; //TODO
-            std::shared_ptr<rviz_rendering::Shape> indicator1_;
+            std::shared_ptr<rviz_rendering::Shape> indicator1_goal_pos_ball_;
+            std::shared_ptr<rviz_rendering::BillboardLine> indicator2_line_to_floor_;
+            std::shared_ptr<rviz_rendering::BillboardLine> indicator3_line_from_drone_;
             rclcpp::Subscription<tf2_msgs::msg::TFMessage>::SharedPtr subscription_;
             
-            float map_height;
+            float fixed_plane_z;
+            Ogre::Vector3 drone_pos;
+            Ogre::Vector3 goal_pos;
+            bool is_active;
 
             void incomingMessage(const tf2_msgs::msg::TFMessage& msg);
+
+            void redraw_indicators();
     };
 }
 
--- src/rviz_enhanced_gui_plugins/src/rviz_enhanced_gui_plugins/rts_pose_tool/rts_pose_tool.cpp
+++ src/rviz_enhanced_gui_plugins/src/rviz_enhanced_gui_plugins/rts_pose_tool/rts_pose_tool.cpp
@@ -33,16 +33,18 @@
 
 namespace rviz_enhanced_gui_plugins {
     RtsPoseTool::RtsPoseTool()
-    : rviz_common::Tool(), indicator1_(nullptr) {
+    : rviz_common::Tool(), indicator1_goal_pos_ball_(nullptr), indicator2_line_to_floor_(nullptr),
+    indicator3_line_from_drone_(nullptr) {
         projection_finder_ = std::make_shared<rviz_rendering::ViewportProjectionFinder>();
         scene_node_ = nullptr;
-        map_height = 0;
+        fixed_plane_z = 0;
+        is_active = false;
+        goal_pos = Ogre::Vector3(0,0,0);
     }
 
     RtsPoseTool::~RtsPoseTool() = default;
 
     void RtsPoseTool::onInitialize() {
-        // TODO node should almost definitely be stored
         rclcpp::Node::SharedPtr node =
             context_->getRosNodeAbstraction().lock()->get_raw_node();
         subscription_ = node->create_subscription<tf2_msgs::msg::TFMessage>(
@@ -50,43 +52,70 @@ namespace rviz_enhanced_gui_plugins {
 
 
         scene_node_ = scene_manager_->getRootSceneNode()->createChildSceneNode();
-        indicator1_ = std::make_shared<rviz_rendering::Shape>(
+        indicator1_goal_pos_ball_ = std::make_shared<rviz_rendering::Shape>(
             rviz_rendering::Shape::Sphere, scene_manager_, scene_node_
         );
-        indicator1_->setColor(0.0f, 1.0f, 0.0f, 1.0f);
-        indicator1_->setScale(Ogre::Vector3(0.1, 0.1, 0.1));
+        indicator1_goal_pos_ball_->setColor(COLOR1_R, COLOR1_G, COLOR1_B, 0.6f);
+        indicator1_goal_pos_ball_->setScale(Ogre::Vector3(0.1, 0.1, 0.1));
+
+        indicator2_line_to_floor_ = std::make_shared<rviz_rendering::BillboardLine>(
+            scene_manager_, scene_node_
+        );
+        indicator3_line_from_drone_ = std::make_shared<rviz_rendering::BillboardLine>(
+            scene_manager_, scene_node_
+        );
+        indicator2_line_to_floor_->setLineWidth(0.02);
+        indicator2_line_to_floor_->setColor(COLOR2_R, COLOR2_G, COLOR2_B, 0.6f);
+        indicator3_line_from_drone_->setLineWidth(0.02);
+        indicator3_line_from_drone_->setColor(COLOR2_R, COLOR2_G, COLOR2_B, 0.6f);
         scene_node_->setVisible(false);
     }
 
     void RtsPoseTool::activate() {
         scene_node_->setVisible(true);
+        is_active = true;
     }
 
     void RtsPoseTool::deactivate() {
         scene_node_->setVisible(false);
+        is_active = false;
     }
 
     int RtsPoseTool::processMouseEvent(rviz_common::ViewportMouseEvent & event) {
         auto point_projection_on_xy_plane = projection_finder_->getViewportPointProjectionOnXYPlane(
             event.panel->getRenderWindow(), event.x, event.y);
         auto position = Ogre::Vector3(point_projection_on_xy_plane.second);
-        position.z = map_height;
-        indicator1_->setPosition(position);
+        position.z = drone_pos.z;
+        goal_pos = position;
+        redraw_indicators();
         return 0;
     }
 
     void RtsPoseTool::incomingMessage(const tf2_msgs::msg::TFMessage& msg) {
-    //    setstate("Message Received!");
         if (msg.transforms[0].header.frame_id == "map"
             && msg.transforms[0].child_frame_id ==   "odom"
+            && rviz_common::validateFloats(msg.transforms[0].transform.translation.x)
+            && rviz_common::validateFloats(msg.transforms[0].transform.translation.y)
             && rviz_common::validateFloats(msg.transforms[0].transform.translation.z)) {
-            map_height = msg.transforms[0].transform.translation.z;
-            auto position = indicator1_->getPosition();
-            position.z = map_height;
-            indicator1_->setPosition(position);
-           // context_->queueRender();
             
+            drone_pos = Ogre::Vector3(
+                msg.transforms[0].transform.translation.x,
+                msg.transforms[0].transform.translation.y,
+                msg.transforms[0].transform.translation.z);
+            redraw_indicators();
         }
+    }
+
+    void RtsPoseTool::redraw_indicators() {
+        indicator1_goal_pos_ball_->setPosition(goal_pos);
+
+        indicator2_line_to_floor_->clear();
+        indicator2_line_to_floor_->addPoint(goal_pos);
+        indicator2_line_to_floor_->addPoint(Ogre::Vector3(goal_pos.x, goal_pos.y, 0));
+
+        indicator3_line_from_drone_->clear();
+        indicator3_line_from_drone_->addPoint(goal_pos);
+        indicator3_line_from_drone_->addPoint(drone_pos);
     }
 }
 
