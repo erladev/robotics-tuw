9b7065518e86a1f10fe34f6eb63bffdf564307b0
laurin <laurin@erlacher.dev>
2025-06-23 19:40:41 +0200

    dji tello camera calibration

---
+++ src/opencv-calibrate/to_stella_config.py
@@ -0,0 +1,32 @@
+import yaml
+import sys
+
+with open(sys.argv[1]) as f:
+    f.readline()
+    data = yaml.safe_load(f)
+
+K = data['K']['data']
+D = data['D']['data']
+
+fx, _, cx, _, fy, cy, _, _, _ = K
+k1,k2,p1,p2,k3 = D
+
+camera_config = {
+    'fx': fx,
+    'fy': fy,
+    'cx': cx,
+    'cy': cy,
+    'k1': k1,
+    'k2': k2,
+    'k3': k3,
+    'p1': p1,
+    'p2': p2,
+
+}
+
+with open("stella_base_config.yaml", "r") as f:
+    config = yaml.safe_load(f)
+    config['Camera'].update(camera_config)
+
+with open("stella_config_filled.yaml", "w") as f:
+    yaml.dump(config, f)
