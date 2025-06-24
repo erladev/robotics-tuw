74ee90b4c3af9387321249f6bb69f89720974a97
laurin <laurin@erlacher.dev>
2025-06-24 18:12:16 +0200

    fix most severe bugs in command unit

--- src/command_unit/command_unit/command_node.py
+++ src/command_unit/command_unit/command_node.py
@@ -1,20 +1,86 @@
 import rclpy
 from rclpy.node import Node
-
+import queue
+import threading, time
+from tf2_ros.buffer import Buffer
+from tf2_ros.transform_listener import TransformListener
 from drone_system_msgs.msg import DroneCommand
 import command_unit.monolithic_command as monolithic_command
+from drone_system_msgs.msg import DroneCommand, DroneCommandFlags
+from drone_system_msgs.srv import DroneInterfaceCommand
+
 
 class CommandNode(Node):
-        def __init__(self):
-            super().__init__("command_node")
-            self.command = monolithic_command.Control(self)
+    def __init__(self):
+        super().__init__("command_node")
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self)
+        
+        self.action_sub = self.create_subscription(
+            DroneCommand,
+            '/gui/actions',
+            self.on_action_msg,
+            2)
+        self.flags_pub = self.create_publisher(DroneCommandFlags, '/cmd/flags', 10)
+        self.drone_if = self.create_client(DroneInterfaceCommand, '/drone/cmd')
+        self.logger = self.get_logger()
+        self.queue = queue.Queue()
+        self.flag_preemption = False
+        self.flag_worker_shutdown = False
+        self.command = monolithic_command.MonolithicControl(self)
 
+        threading.Thread(target=self.command_worker).start()
+    
+
+    # TODO delegate to worker thread
+    def on_action_msg(self, msg):
+        if self.command.is_preemptive(msg):
+            clear_queue(self.queue)
+            self.flag_preemption = True
+        self.queue.put(msg)
+
+    # return True=success, False=fail, None=preempt
+    def call_drone_if(self, command):
+        future = self.drone_if.call_async(command)
+        while True:
+            if future.done():
+                response = future.result()
+                return response.status == DroneInterfaceCommand.Response.STATUS_SUCCESS
+            elif self.flag_preemption:
+                self.logger.info("drone interface call preempted.")
+                return None
+            time.sleep(0.05)
+
+    def command_worker(self):
+        self.logger.info("command worker loop started.")
+        while True:
+            command = self.queue.get()
+            self.flag_preemption = False
+            # shutdown
+            if self.flag_worker_shutdown:
+                self.logger.info("command worker loop terminating.")
+                break
+            self.logger.info("worker loop processing command.")
+            self.command.on_action_msg(command)
+
+def clear_queue(q):
+    try:
+        while True:
+            q.get_nowait()
+    except queue.Empty:
+        pass
 
 def main():
     rclpy.init()
     node = CommandNode()
-    rclpy.spin(node)
-    rclpy.shutdown()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt: pass
+    finally:
+        node.flag_worker_shutdown = True
+        node.flag_preemption = True
+        node.queue.put(None)
+        node.destroy_node()
 
 if __name__ == '__main__':
     main()
--- src/command_unit/command_unit/monolithic_command.py
+++ src/command_unit/command_unit/monolithic_command.py
@@ -3,8 +3,6 @@ from enum import Enum
 import rclpy
 
 from tf2_ros import TransformException
-from tf2_ros.buffer import Buffer
-from tf2_ros.transform_listener import TransformListener
 import tf2_geometry_msgs
 
 from std_msgs.msg import String as StringMsg
@@ -13,26 +11,15 @@ from drone_system_msgs.msg import DroneCommand, DroneCommandFlags
 from drone_system_msgs.srv import DroneInterfaceCommand
 import threading, time
 
-class Control:
+class MonolithicControl:
     def __init__(self, node):
+        self.node = node
+
         self.offline=False # simulate
         self.not_airborne=False # simulate takeoff TODO
         self.emergency_set=False
 
-        self.tf_buffer = Buffer()
-
-        self.node = node
-        self.tf_listener = TransformListener(self.tf_buffer, node)
-        #node.create_subscription()
         self.logger = node.get_logger()
-        self.action_sub = node.create_subscription(
-            DroneCommand,
-            '/gui/actions',
-            self.on_action_msg,
-            2)
-        self.flags_pub = node.create_publisher(DroneCommandFlags, '/cmd/flags', 10)
-        self.drone_if = node.create_client(DroneInterfaceCommand, '/drone/cmd')
-        self.clear_emerg_timer = None
         self.publish_flags()
     
     def publish_flags(self):
@@ -40,7 +27,7 @@ class Control:
         msg.not_airborne = self.not_airborne
         msg.emergency = self.emergency_set
         msg.not_connected = self.offline
-        self.flags_pub.publish(msg)
+        self.node.flags_pub.publish(msg)
 
     def is_exception_state(self):
         return self.offline or self.not_airborne or self.emergency_set
@@ -52,64 +39,64 @@ class Control:
                 if not (self.offline or self.emergency_set):
                     req = DroneInterfaceCommand.Request()
                     req.action = DroneInterfaceCommand.Request.DRONE_TAKEOFF
-                    if self.call_drone_if(req):
+                    r = self.node.call_drone_if(req)
+                    if r is not False: # t/o likely succeeds even if interrupted
                         self.not_airborne = False
-                        return True
+                    return r
                 return False
             case DroneCommand.ACTION_LAND:
                 if not (self.offline or self.not_airborne or self.emergency_set):
                     req = DroneInterfaceCommand.Request()
                     req.action = DroneInterfaceCommand.Request.DRONE_LAND
-                    if self.call_drone_if(req):
+                    r = self.node.call_drone_if(req)
+                    if r:           # land likely wont succeed if preempted
                         self.not_airborne = True
-                        return True
+                        return r
                 return False
             case DroneCommand.ACTION_EMERG:
                 if not self.offline:
                     req = DroneInterfaceCommand.Request()
                     req.action = DroneInterfaceCommand.Request.DRONE_EMERG
-                    self.call_drone_if(req)
+                    r = self.node.call_drone_if(req)
+                    if not r: self.logger.warning("emergency call failed: " + str(r))
                     self.emergency_set = True
-                    self.clear_emerg_timer = self.node.create_timer(1, self.clear_emerg, callback_group=None)
+                    clear_emerg_timer = []
+                    clear_emerg_timer.append(self.node.create_timer(1, lambda: self.clear_emerg(clear_emerg_timer), callback_group=None))
                     return True
                 else: return False
             case DroneCommand.ACTION_STOP:
                 if not (self.offline or self.emergency_set):
                     req = DroneInterfaceCommand.Request()
-                    req.action = DroneInterfaceCommand.Request.DRONE_EMERG
-                    return self.call_drone_if(req)
+                    req.action = DroneInterfaceCommand.Request.DRONE_STOP
+                    return self.node.call_drone_if(req)
                 else:
                     return False
             case DroneCommand.ACTION_MOVE:
                 if self.is_exception_state():
                     return False
-                return self.abs_move_cmd(command)
+                return self.abs_move_cmd(command) # return handled in method
             case DroneCommand.ACTION_ALTITUDE:
                 if self.is_exception_state():
                     return False
                 return self.abs_move_cmd(command, only_altitude=True)
 
-                
-
-
         def __str__(self):
             return str(self.__dict__)
 
-    def call_drone_if(self, command):
-        response = self.drone_if.call(command)
-        return response.status == DroneInterfaceCommand.Response.STATUS_SUCCESS
+    def is_preemptive(self, msg):
+        return msg.action in (DroneCommand.ACTION_STOP, DroneCommand.ACTION_EMERG)
 
     def on_action_msg(self, msg):
         s_ok = self.on_action(msg)
         self.publish_flags()
         self.logger.info(f"Command {msg.action} is ok: {s_ok}")
 
-    def clear_emerg(self):
+    def clear_emerg(self, own_timer):
         self.logger.info("Clear emergency")
         self.emergency_set = False
         self.not_airborne = True
         self.publish_flags()
-        self.clear_emerg_timer.destroy()
+        own_timer[0].destroy()
     
     def abs_move_cmd(self, msg, only_altitude=False):
         pose = geometry_msgs.msg.Pose()
@@ -117,7 +104,6 @@ class Control:
         pose.position.y = msg.y
         pose.position.z = msg.z
         # leave out quaternion
-
         to_frame = "odom"
         from_frame = "map"
         
@@ -125,7 +111,7 @@ class Control:
             self.logger.info("Cannot follow move command due to exception state "+str(self))
             return False
         try:
-            transform = self.tf_buffer.lookup_transform(
+            transform = self.node.tf_buffer.lookup_transform(
                 to_frame,
                 from_frame,
                 rclpy.time.Time())
@@ -133,14 +119,14 @@ class Control:
             self.logger.info(f"issue relative move command: {pose_rel}")
             req = DroneInterfaceCommand.Request()
             req.action = DroneInterfaceCommand.Request.DRONE_MOVE
-            if only_altitude:
+            if not only_altitude:
                 req.x = pose_rel.position.x * 100
                 req.y = pose_rel.position.y * 100
             req.z = pose_rel.position.z * 100
             if abs(req.x) < 20 and abs(req.y) < 20 and abs(req.z) < 20:
                 self.logger.info(f"cannot move, distance too small: {pose_rel}")
                 return False
-            return self.call_drone_if(req)
+            return self.node.call_drone_if(req)
 
         except TransformException as ex:
             self.logger.info(
